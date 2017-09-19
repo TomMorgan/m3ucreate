897bcfb1b9e29f404ecc3d99a02f7639c48164b3
Thomas Morgan <twpmorgan@gmail.com>
2017-09-19 09:34:22 -0500

    added support for joining existing m3us along with any combination of filenames, wildcards into a new m3u

--- m3ucreate.c
+++ m3ucreate.c
@@ -9,11 +9,12 @@ int doM = 0;				 // Is 1 if "-m" is selected, the command to write to the "m3u" 
 int doF = 0;                             // Is 1 if "-f" is given as a command line argument, the command to write to an m3u file given as the last 		
 					 // command line argument. 
 int doA = 0;                             // Is 1 if "-a" is selected, the command to _APPEND_ to an existing m3u file.
+int doJ = 0;                             // Is 1 if "-j" is selected, the command to join the contents of a list of m3us or tracks or any     						 // combination of both
 int overrideM3UCheck = 0;                // Is 1 if "-o" is selected (write to last argument given even if it isn't an m3u file).
 					 // Usage of "-o" can overwrite files so be careful!
 int cmdI = 0;                            // Used to iterate through the command line arguments like "-f" or "-m" or "-o"
 int noCMD = 0;
-const char *version = "0.2";		                           // Used to specify whether a command line option has been entered.
+const char *version = "0.2.1";		                           // Used to specify whether a command line option has been entered.
 
 int main (int argc, char *argv[]) {
 
@@ -43,6 +44,9 @@ int main (int argc, char *argv[]) {
 					break;
 				case 'a':
 					doA=1;
+					break;
+				case 'j':
+					doJ=1;
 					break;
 				case '-':
 					if (strcmp((char *) argv[cmdI] + 2, "help") == 0) {
@@ -208,7 +212,95 @@ int main (int argc, char *argv[]) {
 			
 		}
 
+		if (doJ == 1 || (isM3U(argv[argc - 1]) == 1)) {
 	
+			if (argc > cmdI) {
+				if (((isM3U(argv[argc - 1]) == 1) || (overrideM3UCheck == 1))) {
+					
+					
+					
+					
+					FILE *m3uToWrite;
+					m3uToWrite = fopen(argv[argc - 1], "w");
+
+					if (m3uToWrite == NULL) {
+						fprintf(stderr, "File error with m3u to write");
+						return(-1);
+					}
+
+					for (int i = cmdI; i < argc - 1; i++) {
+						if (isM3U(argv[i]) == 1) {
+							//if (i == cmdI) {
+								FILE *m3uToRead;
+								char m3uStringBuf[32729];
+								
+								m3uToRead = fopen(argv[i], "r");
+								if (m3uToRead == NULL) {
+									fprintf(stderr, "M3UToRead cannot be read");
+									return(-1);
+								}
+ 
+								
+
+								int j = 0;
+
+								while(!(feof(m3uToRead))) {
+									m3uStringBuf[j] = fgetc(m3uToRead);
+									j++;
+								}
+		
+								m3uStringBuf[j - 1] = '\0';
+								
+								fprintf(m3uToWrite, "%s", m3uStringBuf);
+								fclose(m3uToRead);
+								
+						//	}
+						}
+						
+						
+
+						else {
+							int asteriskInFilename = 0;
+					
+				
+							for (char j = 0; j < strlen(argv[i]); j++) {
+							
+								if (argv[i][j] == '*') {                       // Check if there's an asterisk wildcard
+								
+									asteriskInFilename = 1;
+									glob_t globbuf;                         // Use glob to store all instances of wild
+														// card in globbuf
+								
+								//	globbuf.gl_offs = 16384;                // Buffer overflow vulnerability
+									if (glob(argv[i], 0, NULL, &globbuf) != 0) {
+										printf("Problem with glob() or filesystem\n");
+									}
+								
+									for (int k = globbuf.gl_pathc; k > 0; k--) { // Loop through globbuf.gl_pathv 													             // backwards to get alphabetized list
+														     // and print it to m3ufile.	
+										fprintf(m3uToWrite, "%s\n", globbuf.gl_pathv[k]);
+									}
+									break;
+							
+								}
+										
+							}
+
+							if (asteriskInFilename == 0) {
+								fprintf(m3uToWrite, "%s\n", argv[i]);
+							}
+						}
+
+					}
+							
+
+				}				
+								
+			}						
+
+		}									
+							//memptr = (char *) malloc(sizeof(char) * strlen(argv[i]));
+
 	}
 		
 
@@ -239,6 +331,8 @@ void printHelp() {
 		printf("-m plus files outputs to stdout\n");
 		printf("-f plus music files followed by m3ufile outputs to m3ufile. Wildcards accepted.\n");
 		printf("-o overrides requirement that m3ufile for \"-f\" ends in \"m3u\" or \"m3u8\"\n");
-		printf("-a plus music files followed by existing m3ufile appends tracks to m3ufile.\n\n");
+		printf("-a plus music files followed by existing m3ufile appends tracks to m3ufile.\n");
+		printf("-j joins any combination of tracks, wildcards, and existing m3u files into an m3u file\n");
+		printf("   supplied as the final argument.\n\n");
 
 }
